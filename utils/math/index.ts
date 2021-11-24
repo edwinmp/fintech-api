f5f73b8dd58c4d870d9fbc1a03fabe20229b9c94
Edwin P. Magezi <emagezi@gmail.com>
2021-11-24 14:02:12 +0300

    Update calculateAPY to use yearly compound times

--- utils/math/index.ts
+++ utils/math/index.ts
@@ -1,7 +1,9 @@
 import { APYCalculation } from '..';
 
 export const calculateAPY = (request: APYCalculation) => {
-  const value = request.deposit * (Math.pow(1 + request.interest_rate / 100 / 12, 12) - 1);
+  // should probably use a precision library like numbers.js
+  const { deposit, interest_rate, yearly_compound_times } = request;
+  const value = deposit * (Math.pow(1 + interest_rate / 100 / yearly_compound_times, 12) - 1);
 
   return value;
 };
