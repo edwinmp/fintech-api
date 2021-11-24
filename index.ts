2a7554ad20aa78cfa54b671e71aeaf2c67c07eb8
Edwin P. Magezi <emagezi@gmail.com>
2021-11-24 10:23:46 +0300

    Create database tables

--- index.ts
+++ index.ts
@@ -1,15 +1,6 @@
 import express, { Application, Request, Response } from 'express';
-import path from 'path';
-import sqlite3 from 'sqlite3';
+import db from './connection';
 
-const sqlite = sqlite3.verbose();
-const db_name = path.join(__dirname, 'data', 'app.db');
-const db = new sqlite.Database(db_name, (err) => {
-  if (err) {
-    return console.error(err.message);
-  }
-  console.log("Successful connection to the database 'app.db'");
-});
 console.log(db);
 
 const app: Application = express();
